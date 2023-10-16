5a02178769ad9c53324056f768f0ef57a10a29fe
Durlabh-Biswas <84026029+Durlabh-Biswas@users.noreply.github.com>
2023-10-16 13:24:45 +0530

    Update Customer_CMDS.py

--- Customer_CMDS.py
+++ Customer_CMDS.py
@@ -1,1 +1,18 @@
-
+def BookRoom():
+  name=input("Name of Passenger: ")
+  age=input("Age: ")
+  cls=input(""" Suite: 
+  (D-Deluxe\n E-Executive\n L-Luxury)""")
+  occ= int(input("No. of people: "))
+  print("Check In Date(dd-mm-yyyy): ")
+  dd1, mm1, yy1 = map(str, input().split('-'))
+  print("Check In Date(dd-mm-yyyy): ")
+  dd2, mm2, yy2 = map(str, input().split('-'))
+  L=[]
+  while len(L)!=5:
+      n=random.randint(10000,99999)
+      if n not in L:
+          L.append(n)
+  P=n
+  CR.execute(f"INSERT INTO Passenger values('{P}','{name}','{age}','{cls}','{occ}','{yy1}-{mm1}-{dd1}','{yy2}-{mm2}-{dd2}')")
+  DB.commit()
