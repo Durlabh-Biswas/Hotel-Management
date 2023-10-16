9eac7df36d3f30e97e5b71e3d4f37a970094b720
Durlabh-Biswas <84026029+Durlabh-Biswas@users.noreply.github.com>
2023-10-16 13:11:03 +0530

    Create hotel.py

---
+++ hotel.py
@@ -0,0 +1,11 @@
+import random
+import pymysql
+from Customer_CMDS import *
+DB= pymysql.connect(host='localhost',user='t1',password='x')
+CR=DB.cursor()
+CR.execute(f"CREATE DATABASE Hotel_Taj")
+CR.execute(f"use Hotel_Taj")
+CR.execute(f"create table Customer(PID int(5), PName varchar(255), Age int, SUITE char(1), CHECKIN date, CHECKOUT date,ROOMNO int)")
+CR.execute(f"create table Admin(ROOMNO int, AVAILABILITY char(1), SUITE char(1), PID int(5), CHECKIN date, CHECKOUT date)")
+CR.execute(f"use Hotel_Taj")
+
